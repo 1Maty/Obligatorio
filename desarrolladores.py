1301977ae15d8f2f16f66e944f6debef441d9982
Coru29 <105456547+Coru29@users.noreply.github.com>
2022-10-30 20:34:27 -0300

    Add files via upload

---
+++ desarrolladores.py
@@ -0,0 +1,10 @@
+class Desarroladores():
+    def __init__(self, cedula, name, pais_origen, nacimiento, experiencia, rol):
+        self.cedula = cedula
+        self.name = name
+        self.pais_origen = pais_origen
+        self.nacimiento = nacimiento
+        self,experiencia = experiencia
+        self.rol = rol
+
+        
