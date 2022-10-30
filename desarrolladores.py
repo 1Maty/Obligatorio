090bb259e148f49f30ebb66cedd61f43af435ea7
Coru29 <105456547+Coru29@users.noreply.github.com>
2022-10-30 20:52:21 -0300

    Update desarrolladores.py

--- desarrolladores.py
+++ desarrolladores.py
@@ -1,10 +1,44 @@
 class Desarroladores():
-    def __init__(self, cedula, name, pais_origen, nacimiento, experiencia, rol):
+    def __init__(self, cedula, name, pais_origen, nacimiento, experiencia):
         self.cedula = cedula
         self.name = name
         self.pais_origen = pais_origen
         self.nacimiento = nacimiento
-        self,experiencia = experiencia
-        self.rol = rol
+        self.experiencia = experiencia
 
-        
+
+class Diseñador(Desarroladores):
+    def __init__(self, cedula, name, pais_origen, nacimiento, experiencia):
+        super().__init__(cedula, name, pais_origen, nacimiento, experiencia)
+        self.rol = "diseñador"
+
+        @property
+        def rol(self):
+            return self.rol
+
+class Programador(Desarroladores):
+    def __init__(self, cedula, name, pais_origen, nacimiento, experiencia):
+        super().__init__(cedula, name, pais_origen, nacimiento, experiencia)
+        self.rol= "programador"
+
+        @property
+        def rol(self):
+            return self.rol
+
+class Productor(Desarroladores):
+    def __init__(self, cedula, name, pais_origen, nacimiento, experiencia):
+        super().__init__(cedula, name, pais_origen, nacimiento, experiencia)
+        self.rol = "productor"
+
+        @property
+        def rol(self):
+            return self.rol
+
+class Tester(Desarroladores):
+    def __init__(self, cedula, name, pais_origen, nacimiento, experiencia):
+        super().__init__(cedula, name, pais_origen, nacimiento, experiencia)
+        self.rol = "tester"
+
+        @property
+        def rol(self):
+            return self.rol
