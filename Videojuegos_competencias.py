a74fc1f11ed7fc0b14feeace8f2a60c726054866
1Maty <108903917+1Maty@users.noreply.github.com>
2022-10-30 22:28:35 -0300

    Delete Videojuegos_competencias.py

--- Videojuegos_competencias.py
+++
@@ -1,9 +0,0 @@
-class Videojuegos:
-    def __init__(self,nombre:str,categorias:list,lista_devs:list):
-        self.nombre=nombre
-        self.categorias=categorias
-        self.lista_devs=lista_devs
-class Competencias:
-    def __init__(self,categoria:list,podio:tuple):
-        self.categoria=categoria
-        self.podio=podio
