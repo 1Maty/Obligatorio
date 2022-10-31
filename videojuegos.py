d9f731fdb5484545c69cdf85f5634bad5ba48f28
Coru29 <105456547+Coru29@users.noreply.github.com>
2022-10-30 22:29:30 -0300

    Add files via upload

---
+++ videojuegos.py
@@ -0,0 +1,5 @@
+class Videojuegos:
+    def __init__(self,nombre:str,categorias:list,lista_devs:list):
+        self.nombre=nombre
+        self.categorias=categorias
+        self.lista_devs=lista_devs
