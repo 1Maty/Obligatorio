d144297583477b0a77ec190437ddf47b243df1c7
Coru29 <105456547+Coru29@users.noreply.github.com>
2022-11-16 14:58:25 -0300

    Add files via upload

--- desarrolladores.py
+++ desarrolladores.py
@@ -7,8 +7,31 @@ class Desarroladores():
         self._experiencia = experiencia
 
     @property
-    def nombre(self):
-        return(self._name)
+    def name(self):
+        return self._name
+
+    @property
+    def cedula(self):
+        return self._cedula
+
+    @property
+    def experiencia(self):
+        return self._experiencia
+
+    @property
+    def nacimiento(self):
+        return self._nacimiento
+
+    @property
+    def pais(self):
+        return self._pais_origen
+
+    def __eq__(self, otro):
+        return self.cedula == otro.cedula
+
+
+    def __gt__(self,otro):
+        return self.experiencia > otro.experiencia
 
 
 class Diseñador(Desarroladores):
@@ -16,33 +39,49 @@ class Diseñador(Desarroladores):
         super().__init__(cedula, name, pais_origen, nacimiento, experiencia)
         self._rol = "diseñador"
 
-        @property
-        def rol(self):
-            return self._rol
+    @property
+    def rol(self):
+        return self._rol
+    
+    @property
+    def cedula(self):
+        return self._cedula
 
 class Programador(Desarroladores):
     def __init__(self, cedula, name, pais_origen, nacimiento, experiencia):
         super().__init__(cedula, name, pais_origen, nacimiento, experiencia)
         self._rol= "programador"
 
-        @property
-        def rol(self):
-            return self._rol
+    @property
+    def rol(self):
+        return self._rol
+    
+    @property
+    def cedula(self):
+        return self._cedula
 
 class Productor(Desarroladores):
     def __init__(self, cedula, name, pais_origen, nacimiento, experiencia):
         super().__init__(cedula, name, pais_origen, nacimiento, experiencia)
         self._rol = "productor"
 
-        @property
-        def rol(self):
-            return self._rol
+    @property
+    def rol(self):
+        return self._rol
+    
+    @property
+    def cedula(self):
+        return self._cedula
 
 class Tester(Desarroladores):
     def __init__(self, cedula, name, pais_origen, nacimiento, experiencia):
         super().__init__(cedula, name, pais_origen, nacimiento, experiencia)
         self._rol = "tester"
 
-        @property
-        def rol(self):
-            return self._rol
+    @property
+    def rol(self):
+        return self._rol
+    
+    @property
+    def cedula(self):
+        return self._cedula
